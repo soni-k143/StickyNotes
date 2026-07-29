ae108173014e2abf28817938810326b3b3cf7d65
Soni Kumari <soni.kumari@mapmyindia.com>
2026-07-29 10:37:34 +0530

    Introducing local storage using zustand's middleware persist

--- store/notesStore.ts
+++ store/notesStore.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 import { Note } from "@/types/note";
 
 interface NotesState {
@@ -7,21 +9,28 @@ interface NotesState {
   addNote: (title: string, content: string) => void;
 }
 
-export const useNotesStore = create<NotesState>((set) => ({
-  notes: [],
+export const useNotesStore = create<NotesState>()(
+  persist(
+    (set) => ({
+      notes: [],
 
-  addNote: (title, content) => {
-    const newNote: Note = {
-      id: crypto.randomUUID(),
-      title,
-      content,
-      color: "#FDE68A",
-      pinned: false,
-      createdAt: Date.now(),
-    };
+      addNote: (title, content) => {
+        const newNote: Note = {
+          id: crypto.randomUUID(),
+          title,
+          content,
+          color: "#FDE68A",
+          pinned: false,
+          createdAt: Date.now(),
+        };
 
-    set((state) => ({
-      notes: [...state.notes, newNote],
-    }));
-  },
-}));
+        set((state) => ({
+          notes: [...state.notes, newNote],
+        }));
+      },
+    }),
+    {
+      name: "sticky-notes-storage",
+    }
+  )
+);
